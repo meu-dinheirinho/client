da17c6ce1ed26c35a646d244a7b707a75ba3c93b
hermes <hermesemmanueldev@gmail.com>
2023-04-06 19:30:28 -0300

    feat: input page login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import {
+  Card,
+  CardBody,
+  FormControl,
+  FormLabel,
+  Input,
+} from '@chakra-ui/react';
 import styles from './styles.module.css';
 
 export default function LoginPage() {
   return (
-    <div className={styles.teste}>LoginPage</div>
+    <div className={styles.teste}>
+      <Card>
+        <CardBody>
+          <FormControl>
+            <FormLabel>Email address</FormLabel>
+            <Input type={'email'} />
+          </FormControl>
+        </CardBody>
+      </Card>
+    </div>
   );
 }
