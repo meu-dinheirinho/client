399da7273ef8094f46e33de3e2498d9945120fff
hermes <hermesemmanueldev@gmail.com>
2023-04-06 19:34:19 -0300

    test: eslint rules

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import {
-  Card,
-  CardBody,
-  FormControl,
-  FormLabel,
-  Input,
-} from '@chakra-ui/react';
 import styles from './styles.module.css';
 
 export default function LoginPage() {
   return (
     <div className={styles.teste}>
-      <Card>
-        <CardBody>
-          <FormControl>
-            <FormLabel>Email address</FormLabel>
-            <Input type={'email'} />
-          </FormControl>
-        </CardBody>
-      </Card>
+      Login
     </div>
   );
 }
